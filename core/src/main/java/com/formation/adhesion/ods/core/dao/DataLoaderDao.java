6837cdaf853558c99b1f8e9a3fa50ef52979ca2b
SAMI <SAMI@pc_s>
2016-08-26 12:35:03 +0200

    sami

--- core/src/main/java/com/formation/adhesion/ods/core/dao/DataLoaderDao.java
+++ core/src/main/java/com/formation/adhesion/ods/core/dao/DataLoaderDao.java
@@ -13,7 +13,7 @@ import com.formation.adhesion.ods.core.model.Statistics;
 public class DataLoaderDao {
 	private StatisticsDao statisticsDao;
 	private LivreDao livreDao;
-//test git TESTéKHZ
+//test git TESTéKHZDDF
 	public LivreDao getLivreDao() {
 		return livreDao;
 	}
